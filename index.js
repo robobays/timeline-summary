055126e1a353c169def834500f35b0d3147194d7
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2025-07-10 11:28:01 +0300

    Store llm response in file

--- index.js
+++ index.js
@@ -8,38 +8,33 @@ const PROMPT = fs.readFileSync("prompt.txt", "utf-8");
 const app = express();
 app.use(express.json());
 
+async function summarize(match, timeline) {
+  const response = await ollama.chat({
+    model: "llama3.1",
+    messages: [
+      { role: "system", content: PROMPT },
+      { role: "user", content: timeline },
+    ],
+  });
+  fs.writeFileSync(match + ".json", JSON.stringify(response.message.content));
+}
+
+app.get("/timeline-summary/:match", (request, response) => {
+  const match = request.params.match;
+  const summary = fs.readFileSync(match + ".json", "utf-8");
+  response.json({ summary: JSON.parse(summary) });
+});
+
 app.post("/timeline-summary", async (request, response) => {
-  const start = Date.now();
-
-  console.log("Request received");
-  console.log("Prompt:", PROMPT);
-  console.log("Timeline:", JSON.stringify(request.body));
-
   try {
-    const timeline = JSON.stringify(request.body);
-    const result = await ollama.chat({
-      model: "llama3.1",
-      messages: [
-        { role: "system", content: PROMPT },
-        { role: "user", content: timeline },
-      ],
-    });
-    response.json({
-      content: result.message.content,
-      millis: Date.now() - start,
-    });
-    console.log("Request processed in", Date.now() - start, "ms");
+    summarize(request.body.match, request.body.timeline);
+    response.json({ message: "OK" });
   } catch (error) {
-    console.log("Request failed in", Date.now() - start, "ms");
     console.error(error);
     response.status(500).json({ error: error.message || "Internal Server Error" });
   }
 });
 
-
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
-// Set server timeout to 10 minutes (600000 ms)
-server.setTimeout(600000);
