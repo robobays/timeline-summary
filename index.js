8d74cd5acb5d18827b798bd7fa6002a9fc4f9c5d
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2025-07-10 22:37:30 +0300

    Use folders to queue summaries

--- index.js
+++ index.js
@@ -2,53 +2,84 @@ import fs from "fs";
 import express from "express";
 import ollama from "ollama";
 
+const FOLDER_TIMELINE = "timeline/";
+const FOLDER_SUMMARY = "summary/";
 const PORT = process.env.PORT || 3000;
 const PROMPT = fs.readFileSync("prompt.txt", "utf-8");
 
 const app = express();
 app.use(express.json());
 
+async function loadModel() {
+  console.log("Loading model...");
+  const result = await ollama.generate({ model: "llama3.1", keep_alive: "24h" });
+  console.log("Model loaded:", result);
+}
+
 async function summarize(match, timeline) {
   console.log(`Summarizing match ${match} with ${timeline.length} timeline events.`);
 
-  const response = await ollama.chat({
+  const response = await ollama.generate({
     model: "llama3.1",
-    messages: [
-      { role: "system", content: PROMPT },
-      { role: "user", content: JSON.stringify(timeline) },
-    ],
+    system: PROMPT,
+    prompt: JSON.stringify(timeline),
+    format: "json",
+    raw: true,
     stream: true,
+    keep_alive: "24h",
   });
 
   const content = [];
   for await (const chunk of response) {
-    if (chunk.message && chunk.message.content) {
-      console.log(":", chunk.message.content);
-      content.push(chunk.message.content);
+    console.log(":", JSON.stringify(chunk));
+    if (chunk.response) {
+      content.push(chunk.response);
     }
   }
-  fs.writeFileSync(match + ".json", content.join(""), "utf-8");
 
-  console.log(response.message);
-  console.log(`Summary for match ${match} saved.`);
+  return content.join("");
+}
+
+async function runSummary() {
+  const matches = fs.readdirSync(FOLDER_TIMELINE);
+
+  for (const match of matches) {
+    const request = JSON.parse(fs.readFileSync(FOLDER_TIMELINE + match, "utf-8"));
+
+    const summary = await summarize(request.match, request.timeline);
+
+    fs.writeFileSync(FOLDER_SUMMARY + match, summary, "utf-8");
+    fs.removeSync(FOLDER_TIMELINE + match);
+
+    console.log("Saved summary:", match);
+  }
+
+  setTimeout(runSummary, 10000);
 }
 
 app.get("/timeline-summary/:match", (request, response) => {
-  const match = request.params.match;
-  const summary = fs.readFileSync(match + ".json", "utf-8");
-  response.json({ summary: JSON.parse(summary) });
+  const match = request.params?.match;
+  const file = FOLDER_SUMMARY + match + ".json";
+
+  if (match && fs.existsSync(file)) {
+    response.json(JSON.parse(fs.readFileSync(file, "utf-8")));
+  } else {
+    response.json({ error: "No match found for: " + match });
+  }
 });
 
-app.post("/timeline-summary", async (request, response) => {
-  try {
-    summarize(request.body.match, request.body.timeline);
-    response.json({ message: "OK" });
-  } catch (error) {
-    console.error(error);
-    response.status(500).json({ error: error.message || "Internal Server Error" });
-  }
+app.post("/timeline-summary/:match", async (request, response) => {
+  const match = request.params?.match;
+  const file = FOLDER_TIMELINE + match + ".json";
+
+  fs.writeFileSync(file, JSON.stringify(request.body), "utf-8");
+
+  response.json({ message: "OK" });
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+
+  loadModel();
+  runSummary();
 });
